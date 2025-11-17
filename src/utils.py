9cdf2e29c7fe4014583aa9702e16e3a18feb79c8
kemtan <waluyatristan@gmail.com>
2025-11-17 13:21:48 +0700

    add ping cmd

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,13 @@
 from discord.ext import commands
 
 def setup(bot):
+    #!test
     @bot.command()
     async def test(ctx):
         await ctx.send("Bot running.")
+
+    #!ping
+    @bot.command()
+    async def ping(ctx):
+        latency = round(bot.latency * 1000)
+        await ctx.send(f"pong ({latency} ms)")
