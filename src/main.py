17fe00f7313a349ac0af655a43642ed8889bc9ec
kemtan <waluyatristan@gmail.com>
2025-11-17 16:05:44 +0700

    fix bot wont send commit

--- src/main.py
+++ src/main.py
@@ -12,17 +12,35 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 utils.setup(bot)
 
-@tasks.loop(minutes=2)
+@tasks.loop(minutes=1)
 async def check_github():
     result = await github.check_new_commit()
+
+    # cached channel
+    channel = bot.get_channel(config.CHANNEL_ID)
+
+    # fetch channel
+    if channel is None:
+        try:
+            channel = await bot.fetch_channel(config.CHANNEL_ID)
+            print("Fetched channel from API:", channel, type(channel))
+        except discord.Forbidden:
+            print("ERROR: Bot has no permission to view/send in this channel")
+            return
+        except discord.HTTPException as e:
+            print("ERROR: Failed to fetch channel:", e)
+            return
+
     if result:
         old_sha, new_sha = result
-        channel = bot.get_channel(config.CHANNEL_ID)
         await channel.send(f"New commit!\nOld: `{old_sha}`\nNew: `{new_sha}`")
+    else:
+        await channel.send("no commit")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+    print(config.CHANNEL_ID)
     print(config.GITHUB_OWNER)
     print(config.GITHUB_REPO)
     check_github.start()
