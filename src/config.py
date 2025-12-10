60687e48a19138b1d21092b79fc859062f8d43da
kemtan <waluyatristan@gmail.com>
2025-12-10 11:40:01 +0700

    add reddit env

--- src/config.py
+++ src/config.py
@@ -6,6 +6,10 @@ load_dotenv()
 DISCORD_TOKEN  = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID     = os.getenv("DISCORD_CHANNEL_ID")
 
+REDDIT_SUBREDDIT  = os.getenv("REDDIT_SUBREDDIT")
+REDDIT_USERNAME   = os.getenv("REDDIT_USERNAME")
+REDDIT_CHANNEL_ID = os.getenv("REDDIT_CHANNEL_ID")
+
 GITHUB_TOKEN   = os.getenv("GITHUB_TOKEN")
 GITHUB_OWNER   = os.getenv("GITHUB_OWNERS")
 GITHUB_REPO    = os.getenv("GITHUB_REPOS")
